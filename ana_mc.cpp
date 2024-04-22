f5a6d1e6d40acc64fcc76fc142816538ce690ca3
robbins <you@example.com>
2024-04-22 16:20:46 +0800

    can sue project

---
+++ ana_mc.cpp
@@ -0,0 +1,154 @@
+#include<iostream>
+#include<cstdio>
+#include<stdio.h>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<map>
+#include<regex>
+#include<mutex>
+#include<thread>
+#include<iomanip>
+
+std::mutex mapmtx;
+std::map<std::string,int> fidmap;
+long long int line_number=0;
+void add_map(std::string element){
+
+	mapmtx.lock();
+	auto id=fidmap.find(element);
+	if(id !=fidmap.end()){
+		fidmap[element]=fidmap[element]+1;
+	}else{
+		std::cout<< element<<" found not in map"<<std::endl;
+		fidmap[element]=1;
+	}
+
+	mapmtx.unlock();
+
+}
+
+
+
+void processFileBlock(const std::string& filename,int startLine,int endLine,int threadID,std::mutex& mtx)
+{
+	mtx.lock();
+
+	std::ifstream file(filename);
+	std::cout<<threadID<<std::endl;
+	if(!file.is_open())
+	{
+		std::cerr<<"Error opening file:"<<filename<<std::endl;
+		return;
+	}
+	//block start position
+	
+	std::string line;
+	std::smatch match;
+	std::regex pattern("([01]{1})(\\s+)([01]{1})(\\s+)([0-9A-Z]{2})(\\s+)([0-9A-Z]{2})");
+	
+	int lineNum=0;
+	while(std::getline(file,line)){
+		if(lineNum>=startLine&&lineNum<endLine){
+	//	mtx.lock();
+		if(regex_search(line,match,pattern))
+		{
+			add_map(match[7]);
+		}
+		else{
+			printf("%d:  %lld line  not match!\n",threadID,line_number);
+		}
+		printf("%d:  %lld lines processored...\n",threadID,line_number++);
+	
+		printf("\033[A");
+		
+		printf("\033[K");
+		
+		//mtx.unlock();
+
+		}
+		++lineNum;
+
+	}	
+	
+	mtx.unlock();
+	file.close();
+
+}
+
+
+int main(int argc,char* argv[])
+{	
+	if(argc<2){
+		std::cerr<<"Usage: "<<argv[0]<<"<filename>"<<std::endl;
+		return 1;
+	}
+        const int numThreads=std::thread::hardware_concurrency();	
+	
+	std::string filename=argv[1];
+	
+	//打开文件并获取文件大小
+	std::ifstream file(filename);
+	std::cout<<filename<<std::endl;
+	if(!file.is_open()){
+		std::cerr<<"Unable to open file."<<std::endl;
+		return 1;
+	}
+
+	int lineCount=0;
+	std::string line1;
+	while(std::getline(file,line1))
+	{
+		++lineCount;
+	}
+	std::cout<<"linecoune"<<lineCount<<std::endl;
+
+	std::streampos fileSize=file.tellg();
+	file.close();
+
+	//计算每个线程处理的文件大小
+/*	std::vector<std::pair<std::streampos,std::streampos>> blocks;
+	std::streampos blockSize = fileSize/ numThreads;
+	for(int i =0;i<numThreads;i++){
+		blocks.emplace_back(i*blockSize,blockSize);
+
+	} */
+
+	std::vector<std::thread> threads;
+	std::mutex mtx;
+	int linePercore=lineCount/numThreads;
+	int startLine=0;
+	int endLine=0;
+	for(int i=0;i<numThreads;i++){
+		startLine=i*linePercore;
+		endLine=(i==numThreads-1)?lineCount:(i+1)*linePercore;
+		threads.emplace_back(processFileBlock,filename,startLine,endLine,i,std::ref(mtx));
+		std::cout<<"core"<<i<<" startline: "<<startLine<<" endline: "<<endLine<<std::endl;
+	}	
+
+	//wait thread return
+	for(auto& thread:threads){
+		thread.join();
+	}
+	std::cout<<"aaa"<<std::endl;
+
+	int whole_count=0;
+	for(const auto& pair:fidmap){
+		whole_count+=pair.second;
+		std::cout<<"whole_count"<<whole_count<<std::endl;
+		
+	}
+	
+	for(const auto& pair:fidmap){
+	
+//	std::cout<<pair.first<<": "<<pair.second<<std::fixed<<std::setprecision(6)<<" "<<pair.second*100.00/33554432LL<<"%"<<std::endl;
+	std::cout<<pair.first<<": ";
+		printf("%5d%10.6f%%\n",pair.second,pair.second*100.00/whole_count);
+	
+	}
+	
+
+	return 0;
+
+}
---
+++ generate_plot.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<cstdio>
+#include<stdio.h>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<map>
+#include<regex>
+
+
+int main(int argc,char* argv[])
+{	
+	std::map<std::string,int> fidmap;
+	
+	std::ifstream file("data.txt");
+	if(!file.is_open()){
+		std::cerr<<"Unable to open file."<<std::endl;
+		return 1;
+	}
+
+	std::string line;
+	std::vector<std::string> firstLine;
+	//first line
+	if(std::getline(file,line)){
+	
+	std::istringstream iss(line);
+	std::string word;
+	while(iss>>word){
+	firstLine.push_back(word);
+		}	
+	}
+//	for(const auto& word:firstLine){
+//		std::cout<<word<<std::endl;
+//	}
+
+	long long int line_number=0;
+	//rest line
+	while(std::getline(file,line))
+	{
+		std::smatch match;
+		std::regex pattern("([01]{1})(\\s+)([0-9A-Z]{2})(\\s+)([0-9A-Z]{2})");
+		if(regex_search(line,match,pattern))
+		{
+		//	std::cout<<match[3]<<"jjj"<<match[5]<<std::endl;
+			auto id=fidmap.find(match[5]);
+			if(id !=fidmap.end()){
+	
+			fidmap[match[5]]=fidmap[match[5]]+1;
+	
+			}else{
+			std::cout<<match[5]<<" found not in map"<<std::endl;
+			fidmap[match[5]]=1;
+			}
+		}
+		else{
+		printf("%lld line  not match!\n",line_number);
+		}
+		printf("%lld lines processored...\n",line_number++);
+	//	fflush(stdout);
+		// \033 对应ASCII ESC控制码，ESC[：CSI(control sequence introducer)
+		printf("\033[A");
+		//
+		printf("\033[K");
+	}
+	
+
+	
+	int whole_count=0;
+	for(const auto& pair:fidmap){
+		whole_count+=pair.second;
+	}
+	for(const auto& pair:fidmap){
+//	std::cout<<pair.first<<": "<<pair.second<<" "<<pair.second/whole_count*100<<"%"<<std::endl;
+		printf("%d: %-5d\t%8.2lf\n",pair.first,pair.second,pair.second/whole_count);
+	}
+	
+	file.close();
+
+
+	return 0;
+
+}
